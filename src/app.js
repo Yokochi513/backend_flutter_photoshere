1a7aa3b086271cf6d584394645d0b02ad034086d
yokochi <91511674+Yokochi513@users.noreply.github.com>
2025-11-27 18:44:05 +0900

    [Add] 写真データ取得用の設定を追加

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
+import path from "path";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/api", routes);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 export default app;
