3ba842c438a02ce1593e2c7fee59b09b943d46c8
yokochi <yokochi5123@gmail.com>
2025-12-17 08:06:07 +0900

    [Mod] ディレクトリの自動作成を行うように変更

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes/index.js";
 import path from "path";
+import fs from "fs";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
+
+if (!fs.existsSync("uploads")){
+    fs.mkdirSync("uploads");
+}
+
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 export default app;
